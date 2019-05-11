029134e36ad35231e51d7628697d2652be31d604
tomoaki-kimura <tkim19750404@gmail.com>
2019-05-11 15:24:09 +0900

    add ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,25 +4,30 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
-  def create
+  def
+
+
+    create
     if params[:question_id]
       @question = Question.find(params[:question_id])
       answer = answer_select(@question, params[:answer])
       # binding.pry
-      Message.create!(content: answer)
+      @message_user = Message.create!(content: answer)
       @message = Message.new(content: "<p>あなたの答えは<span class='text-success'><strong>#{answer}</strong></span>です。</p>", answer: params[:answer], question_id: @question.id, robo: true)
     else
       @message = Message.new(message_params)
     end
 
     if @message.save && @message.content == "quiz"
-      Message.create!(content: 'クイズです！！',question_id: Question.pluck(:id).sample, robo: true)
-      redirect_to root_path
+      @message_robo = Message.create!(content: 'クイズです！！',question_id: Question.pluck(:id).sample, robo: true)
+      render :create
     elsif @message.save && params[:message_id].present?
-      Message.find(params[:message_id]).update!(finished: true)
-      redirect_to root_path
+      puts @message.inspect
+      @message_finished = Message.find(params[:message_id])
+      @message_finished.update!(finished: true)
+      render :create
     elsif @message.save
-      redirect_to root_path
+      render :create
     else
       @messages = Message.all.order(created_at: :asc)
       render :index
