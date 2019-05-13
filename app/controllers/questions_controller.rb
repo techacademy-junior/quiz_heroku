0ce2255cee177daed6ebe159dc30461779d08798
tomoaki-kimura <tkim19750404@gmail.com>
2019-05-13 11:52:31 +0900

    finish ver

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -55,10 +55,14 @@ class QuestionsController < ApplicationController
   # DELETE /questions/1
   # DELETE /questions/1.json
   def destroy
-    @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
+    begin
+      @question.destroy
+      respond_to do |format|
+        format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    rescue
+      redirect_to questions_path, notice: '使用されたクイズは消せません'
     end
   end
 
